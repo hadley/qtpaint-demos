ab579326958bb6830d2c13ede3d762b67f8999f2
hadley <h.wickham@gmail.com>
2009-06-07 20:57:23 -0500

    Attempt at general interactive wscatterplot

--- scatterplot.r
+++ scatterplot.r
@@ -1,3 +1,30 @@
-# Add basic keyboard interactivity to scatterplot:
-# * up/down controls point size
-# * left/right controls alpha
+# l(qtpaint); source("~/Documents/cranvas/qtpaint/demo/scatterplot.r")
+library(qtpaint)
+
+n <- 10000
+x <- rnorm(n, 50, 25)
+y <- rnorm(n, 50, 25)
+df <- data.frame(X = x, Y = y)
+
+circle <- qvPathCircle(0, 0, 5)
+render_plot <- function(layer, canvas, exposed) {
+  qvFillColor(canvas) <- "red"
+  qvStrokeColor(canvas) <- NA
+  qvGlyph(canvas, circle, df[,1], df[,2])
+}
+
+handle_keys <- function(event) {
+  print(str(event))
+  browser()
+}
+
+scene <- qvScene()
+root <- qvLayer(scene)
+
+points <- qvLayer(root, render_plot, keyPressFun = handle_keys)
+qvSetLimits(points, range(df$X), range(df$X))
+
+view <- qvViewWidget(scene = scene, opengl = FALSE)
+overlay <- qvOverlay(view)
+
+print(view)
