2df960a1de805ae5f6366072c9f1cca43f27f5cb
hadley <h.wickham@gmail.com>
2009-07-08 14:45:56 +0200

    Tweaks to scatterplot

--- scatterplot.r
+++ scatterplot.r
@@ -1,7 +1,7 @@
 # source("~/Documents/cranvas/demos/scatterplot.r")
 library(qtpaint)
 
-n <- 50000
+n <- 1e6
 x <- rnorm(n, 50, 25)
 y <- rnorm(n, 50, 25)
 df <- data.frame(X = x, Y = y)
@@ -9,19 +9,25 @@ df <- data.frame(X = x, Y = y)
 size <- 3
 alpha <- 1
 
-render_plot <- function(layer, canvas, exposed) {
-  circle <- qpathCircle(0, 0, size)
+render_plot <- function(item, painter, exposed) {
 
-  qfillColor(canvas) <- ggplot2::alpha("blue", alpha)
-  qstrokeColor(canvas) <- NA
-  qdrawGlyph(canvas, circle, df[,1], df[,2])
+  
+  if (size < 0.5) {
+    qstrokeColor(painter) <- ggplot2::alpha("blue", alpha)
+    qdrawPoint(painter, df[, 1], df[,2])
+  } else {
+    circle <- qpathCircle(0, 0, size)
+    qfillColor(painter) <- ggplot2::alpha("blue", alpha)
+    qstrokeColor(painter) <- NA
+    qdrawGlyph(painter, circle, df[, 1], df[,2])
+  }
 }
 
 handle_keys <- function(event) {
   if (event$key == "up") {
-    size <<- size + 1
+    size <<- size + 0.5
   } else if (event$key == "down") {
-    size <<- max(size - 1, 1)
+    size <<- max(size - 0.5, 0.5)
   } else if (event$key == "left") {
     alpha <<- max(alpha - 0.05, 0.05)
   } else if (event$key == "right") {
@@ -29,6 +35,8 @@ handle_keys <- function(event) {
   }
   qupdate(scene)
 }
+
+if (exists("view")) qclose(view)
 
 scene <- qgraphicsScene()
 view <- qplotView(scene = scene)
